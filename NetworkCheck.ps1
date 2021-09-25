b7fbf837c7c1f4f423ac377d90674cea161d6319
yokaimeow <yokaimeow@qq.com>
2021-09-25 16:01:30 +0800

    reset uri

--- NetworkCheck.ps1
+++ NetworkCheck.ps1
@@ -1,4 +1,4 @@
 # IP
 Invoke-RestMethod -Uri http://ip-api.com/json/ | Out-File .\result.txt
 # Trace Route
-Test-NetConnection xsl.cokutau.com -TraceRoute | Out-File -Append .\result.txt
+Test-NetConnection baidu.com -TraceRoute | Out-File -Append .\result.txt
